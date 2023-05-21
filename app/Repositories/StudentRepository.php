5b9fbe6d78399422ee1066e30cb9e1f080565481
taimoornaveed <mtaimoornaveed@gmail.com>
2023-05-21 23:15:51 +0500

    working

--- app/Repositories/StudentRepository.php
+++ app/Repositories/StudentRepository.php
@@ -313,6 +313,7 @@ class StudentRepository implements StudentRepositoryInterface
             DB::commit();
             return response()->json(['status' => true, 'msg' => 'Data Saved Successfully']);
         } catch (\Exception $e) {
+            dd($e);
             DB::rollback();
             if ($e->getCode() == 23000 && str_contains($e->getMessage(), 'Duplicate entry')) {
                 $pattern = "/Duplicate entry '.*' for key '(.*?)'/";
